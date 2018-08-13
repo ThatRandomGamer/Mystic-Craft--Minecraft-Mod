f89690baeb15f4d41d4cc769ea5160230a6a5e45
ThatRandomGamer <rudra.g.singh@gmail.com>
2018-08-13 08:16:30 +0530

    Beta 1.2 content! Plus finally setting it up correctly.

---
+++ src/main/java/com/TRGamer28/coppermod/blocks/SilverOre.java
@@ -0,0 +1,25 @@
+package com.TRGamer28.coppermod.blocks;
+
+import java.util.Random;
+
+import com.TRGamer28.coppermod.init.ModItems;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+
+public class SilverOre extends BlockBase {
+
+	public SilverOre(String name, Material material) {
+		super(name, material);
+		setSoundType(SoundType.METAL);
+		setHardness(5.0F);
+		setResistance(15.0F);
+		setHarvestLevel("pickaxe", 2);
+	}
+	
+	
+	
+	 
+}
